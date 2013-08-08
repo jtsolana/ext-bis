0313339e28ef436080306767d81b9bc093bf1e75
jtsolana <jtsolana@gmail.com>
2013-08-08 15:21:15 +0800

    Add-on class view Registration

---
+++ app/view/content/Registration.js
@@ -0,0 +1,9 @@
+Ext.define('ext-bis.view.content.Registration', {
+    extend: 'Ext.panel.Panel',
+    
+    xtype: 'app-registration',
+    itemId: 'registration',
+ 	layout: 'fit'
+ 	html:'Registration Panel'   
+});
+
