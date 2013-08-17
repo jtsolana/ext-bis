0882960600ba5fec7e9fba2d7242327fd6b1e6b7
jtsolana <jtsolana@gmail.com>
2013-08-18 00:08:05 +0800

    Add-on function reate window registration form

--- app/controller/Registration.js
+++ app/controller/Registration.js
@@ -1,6 +1,7 @@
 Ext.define('ext-bis.controller.Registration', {
     extend: 'Ext.app.Controller',	
     refs: [{ ref: 'registration', selector: ''} ],
+    
     init: function() {
 	
         this.control({
@@ -16,57 +17,18 @@ Ext.define('ext-bis.controller.Registration', {
 
     onClickNewRecord: function() {
 
-        var form = Ext.create('Ext.form.Panel', {
-            
-            bodyPadding: '5 5 0',
-            fieldDefaults: {
-                msgTarget: 'side',
-                labelWidth: 75
-            },
-            defaults: { anchor: '100%' },
-
-            items: [{
-                xtype:'fieldset',
-                title: 'Personal Information',
-                collapsible: true,
-                defaultType: 'textfield',
-                layout: 'anchor',
-                defaults: {
-                    anchor: '100%'
-                },
-                items :[{
-                    fieldLabel: 'Home',
-                    name: 'home',
-                    value: '(888) 555-1212'
-                },{
-                    fieldLabel: 'Business',
-                    name: 'business'
-                },{
-                    fieldLabel: 'Mobile',
-                    name: 'mobile'
-                },{
-                    fieldLabel: 'Fax',
-                    name: 'fax'
-                }]
-            }],
-
-            buttons: [{
-                text: 'Save'
-            },{
-                text: 'Cancel'
-            }]
-        });
-
         var window = Ext.create('Ext.window.Window', {
-            title: 'Citizen Information Form',
+            title: 'Registration Form',
             iconCls: '',
             height: 500,
             width: 600, 
             layout: 'fit',   
-            items:[form]  
+            items:[{
+                xtype: 'form-registration'
+            }]  
         }).show();  
 
-    },
+    }
 
 
 });
@@ -80,7 +42,7 @@ Ext.define('ext-bis.controller.Registration', {
                         
 
                 
-                    
-            
-            
+
+                   
+      
                         
